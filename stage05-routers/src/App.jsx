a7e37c4d29d0452be46d3badad2b741846d89f6c
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-09 11:08:50 -0300

    add a new folder with name satage05-routers, this folder is about rourtes in react

---
+++ stage05-routers/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+function App() {
+   return (
+      <>
+         <BrowserRouter>
+            <Header />
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="about" element={<About />} />
+               <Route path="*" element={<NotFound />} />
+            </Routes>
+         </BrowserRouter>
+      </>
+   );
+}
+
+export default App;
---
+++ stage05-routers/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+   return (
+      <div>
+         <h1>Sorry</h1>
+         <p>Page doesn't find</p>
+      </div>
+   );
+};
+
+export default NotFound;
